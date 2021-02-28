23a3c0ed8a88f5990c4b784d692a7e49f2810f21
DOL7JS <53859920+DOL7JS@users.noreply.github.com>
2021-02-28 10:31:10 +0100

    Cv02

---
+++ Cv02/BaseLib/Reading.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Fei
+{
+    namespace BaseLib
+    {
+        public class Reading
+        {
+            /// <summary>
+            /// Metoda nacte hodnotu z konzole jako string a pote zkusi prevest na int
+            /// Vraci prevedeny int nebo null
+            /// </summary>
+            /// <param name="text">Vypis vyzvy k napsani hodnoty</param>
+            /// <returns></returns>
+            public static int? ReadInt(string text)
+            {
+                Console.WriteLine(text + ": ");
+                string input = Console.ReadLine();
+                if (int.TryParse(input, out int result))
+                {
+                    return Convert.ToInt32(result);
+                }
+                return null;
+            }
+            /// <summary>
+            /// Metoda nacte a vraci string
+            /// </summary>
+            /// <param name="text">Vypis vyzvy k napsani hodnoty</param>
+            /// <returns></returns>
+            public static string ReadString(string text)
+            {
+                Console.WriteLine(text + ": ");
+                return Console.ReadLine();
+            }
+            /// <summary>
+            /// Metoda nacte hodnotu z konzole jako string a pote zkusi prevest na double
+            /// Vraci prevedeny double nebo null
+            /// </summary>
+            /// <param name="text">Vypis vyzvy k napsani hodnoty</param>
+            /// <returns></returns>
+            public static double? ReadDouble(string text)
+            {
+                Console.WriteLine(text + ": ");
+                string input = Console.ReadLine();
+                if (double.TryParse(input, out double result))
+                {
+                    return Convert.ToDouble(result);
+                }
+                return null;
+            }
+            /// <summary>
+            /// Metoda vraci char
+            /// </summary>
+            /// <param name="text">Vypis vyzvy k napsani hodnoty</param>
+            /// <returns></returns>
+            public static char ReadChar(string text)
+            {
+                Console.WriteLine(text + ": ");
+                return (char)Console.Read();
+            }
+        }
+    }
+}
---
+++ Cv02/Cv02_odevzdani/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using Fei.BaseLib;
+
+namespace Cv02_odevzdani
+{
+    class Program
+    {
+        private static void LoadArray( ref double[] array) {
+            double? size = Reading.ReadDouble("Vložte velikost pole");
+            if (size != null)
+            {
+                if (size < 1)
+                {
+                    Console.WriteLine("Zadal jsi nekladne cislo pro velikost pole");
+                    return;
+                }
+                array = new double[(int)size];
+                for (int i = 0; i < size; i++)
+                {
+                    double? value = Reading.ReadDouble($"Array[{i}]");
+                    if (value != null)
+                    {
+                        array[i] = (double)value;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nezadal jsi cislo");
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Nezadal jsi cislo");
+                return;
+            }
+        }
+        private static void PrintArray(double[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine($"Index {i} = {array[i]}");
+            }
+        }
+        private static void SortArray( double[]array) {
+            Array.Sort(array);
+            Console.WriteLine("Pole bylo setrideno");
+        }
+        private static void FindMinValue(double[] array) {
+            if (array.Length < 1) {
+                return;
+            }
+            double min = array[0];
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] < min)
+                {
+                    min = array[i];
+                }
+            }
+            Console.WriteLine($"Nejmensi hodnota je {min}");
+        }
+        private static void FindFirstValue(double[] array) {
+            double? number = Reading.ReadDouble("Zadej hledane cislo");
+            if (number != null)
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    if (array[i] == (double)number)
+                    {
+                        Console.WriteLine($"Prvni vyskyt cisla {number} se vyskytuje na indexu {i}");
+                        break;
+                    }
+                }
+            }
+        }
+        private static void FindLastValue(double[] array) {
+            double? number = Reading.ReadDouble("Zadej hledane cislo");
+            if (number != null)
+            {
+                for (int i = array.Length - 1; i >= 0; i--)
+                {
+                    if (array[i] == (double)number)
+                    {
+                        Console.WriteLine($"Posledni vyskyt cisla {number} se vyskytuje na indexu {i}");
+                        break;
+                    }
+                }
+            }
+        }
+        private static void PrintMenu() {
+            Console.WriteLine(    "-------------------------------------------------\n" +
+                                  "1.Zadaní prvků pole z klávesnice\n"
+                                + "2.Výpis pole na obrazovku\n"
+                                + "3.Utřídění pole vzestupně\n"
+                                + "4.Hledání minimálního prvku\n"
+                                + "5.Hledání prvního výskytu zadaného čísla\n"
+                                + "6.Hledání posledního výskytu zadaného čísla\n"
+                                + "7.Konec programu\n");
+        }
+        static void Main(string[] args)
+        {
+          
+            double[] array = new double[0];
+            do {
+                PrintMenu();
+                switch (Reading.ReadInt("Vyberte možnost")) {
+                    case 1:
+                        LoadArray(ref array);
+                        break;
+                    case 2:
+                        PrintArray(array);
+                        break;
+                    case 3:
+                        SortArray( array);
+                        break;
+                    case 4:
+                        FindMinValue(array);
+                        break;
+                    case 5:
+                        FindFirstValue(array);
+                        break;
+                    case 6:
+                        FindLastValue(array);
+                        break;
+                    case 7:
+                        Environment.Exit(0);
+                        break;
+                }
+                Console.WriteLine("Pokračuj kliknutím");
+                Console.ReadKey();
+            } while (true);
+        
+        }
+
+        
+    }
+}
